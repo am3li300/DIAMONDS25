88589f4807c57b47e8b537610bcfb619afee6f1d
yeedaKing <david.yee@ucdenver.edu>
2025-06-11 12:16:22 -0400

    test

--- STRING_to_gene_symbols.py
+++ STRING_to_gene_symbols.py
@@ -2,25 +2,32 @@ import pandas as pd
 
 def main():
 	# Load alias file
-	alias_df = pd.read_csv("9606.protein.aliases.v12.0.txt", sep="\t", header=None, names=["protein_id", "alias", "source"])
+	alias_df = pd.read_csv("data/9606.protein.aliases.v12.0.txt", sep="\t", header=None, names=["protein_id", "alias", "source"])
 
-	# Load nodes_to_expand
-	with open("nodes_to_expand.txt") as f:
-    		seed_ids = [line.strip() for line in f if line.strip()]
+	# Load nodes file
+	file_name = input("Enter tsv file path/name: ")
+	seed_ids = []
+	with open(file_name, 'r') as f:
+		for line in f:
+			protein = line.strip().split()[0]
+			print(protein)
+			seed_ids.append(protein)
+
 
 	# Map to HGNC gene symbols
-	symbol_map = alias_df[(alias_df["protein_id"].isin(seed_ids)) & (alias_df["source"].str.contains("Ensembl_HGNC|BLAST_UniProt_AC|Ensembl_Gene"))]
-	# You can filter for a preferred source if you want only HGNC, for example.
+	symbol_map = alias_df[
+		(alias_df["protein_id"].isin(seed_ids)) &
+		(alias_df["source"].str.contains("Ensembl_HGNC|BLAST_UniProt_AC|Ensembl_Gene"))
+	]
 
 	# Find gene symbols for each protein_id
 	gene_dict = symbol_map.groupby("protein_id")["alias"].apply(list).to_dict()
 
 	with open("STRING_to_gene_symbols.txt", 'w') as file:
 		for pid in seed_ids:
-    			file.write(pid + ': ' + ", ".join(gene_dict.get(pid, [])) + '\n\n')
+			file.write(pid + ': ' + ", ".join(gene_dict.get(pid, [])) + '\n\n')
 
 	print("Saved STRING to gene symbol mapping to STRING_to_gene_symbols.txt")
-
 
 if __name__ == "__main__":
 	main()
