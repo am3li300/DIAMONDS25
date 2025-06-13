1d773ebb0ba66a270933e1e58fa1a6667b5394ee
yeedaKing <david.yee@ucdenver.edu>
2025-06-13 13:41:14 -0400

    Co-authored-by: am3li300 <am3li300@users.noreply.github.com>

---
+++ partition.py
@@ -0,0 +1,31 @@
+from random import shuffle
+
+def partition(path, i, nodes):
+    shuffle(nodes)
+    n = len(nodes)
+    nonseeds = nodes[:n//2]
+    seeds = nodes[n//2:]
+    
+    disease = input("Enter disease: ")
+    with open(path + "/" + disease + "_new_seeds_" + i + ".txt", 'w') as f1:
+        for node in seeds:
+            f1.write(node + '\n')
+    f1.close()
+
+    with open(path + "/" + disease + "_non_seeds_" + i + ".txt", 'w') as f2:
+        for node in nonseeds:
+            f2.write(node + '\n')
+    f2.close()
+
+def main():
+    path = input("Enter folder path to save output: ")
+    fileName = input("Enter seed file name: ")
+    f = open(fileName, 'r')
+    nodes = [ line[:-1] for line in f ]
+    for i in range(25):
+        partition(path, i, nodes)
+
+    print("All done!")
+    
+if __name__ == "__main__":
+    main()
