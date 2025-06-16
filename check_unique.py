dd3297c2501bb9e9e90bf0724368722575c69c34
yeedaKing <david.yee@ucdenver.edu>
2025-06-15 20:11:46 -0400

    check

---
+++ check_unique.py
@@ -0,0 +1,14 @@
+disease = input("Check SZ or RA output: ")
+filePath = "output/validation_rankings/" + ("rheumatoid_arthritis" if disease == "RA" else "schizophrenia") \
+            + f"/{disease}_cross_validation_"
+
+k = int(input("Enter k-value: "))
+seen = set()
+for i in range(25):
+    file = open(f"{fileName}{str(i)}.out")
+    top_k = tuple([line.split()[0] for line in file][-k:])
+    if top_k not in seen:
+        seen.add(top_k)
+
+    else:
+        print(f"File {i} is not unique")
