76e0c1d5f4a5a1794fa6fa68a809696522462008
yeedaKing <david.yee@ucdenver.edu>
2025-06-24 17:40:56 -0400

    humanbase cross validation rankings

--- adagio_standalone.py
+++ adagio_standalone.py
@@ -252,6 +252,6 @@ python3 main.py \
 
 """
 python3 main.py \
-  --network '/Users/dkyee/Desktop/CSCI/DIAMONDS Tufts/DIAMONDS25/data/entrez_filtered_brain_0.3.tsv' \
+  --network '/Users/dkyee/Desktop/CSCI/DIAMONDS Tufts/DIAMONDS25/data/networks/entrez_filtered_brain_0.3.tsv' \
   --genelist '/Users/dkyee/Desktop/CSCI/DIAMONDS Tufts/DIAMONDS25/cross_validation/partitioning/SZ_humanbase/3_schizophrenia_new_seeds_0.txt' --out SZ_cross_validation_0.out
 """
