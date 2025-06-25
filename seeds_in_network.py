64ada528bedfa98d08a6fba0c8421c6dbcffb873
am3li300 <amelie.hopke@tufts.edu>
2025-06-25 16:42:08 -0400

    seeds in network

---
+++ seeds_in_network.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# network assumed to be tsv, gene gene score
+net_path = input("Enter network file path: ")
+seed_path = input("Enter seed file path: ")
+out_path = input("Enter the output path/file name: ")
+
+df = pd.read_csv(net_path, delimiter='\t')
+
+genes = set()
+
+for gene in df.iloc[:, 0]:
+    genes.add(str(gene))
+
+for gene in df.iloc[:, 1]:
+    genes.add(str(gene))
+
+fout = open(out_path, "w")
+with open(seed_path) as seeds:
+    for seed in seeds:
+        seed = seed.strip()
+        if seed in genes:
+            fout.write(seed + '\n')
+
