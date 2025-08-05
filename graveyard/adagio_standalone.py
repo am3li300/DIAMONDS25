a83a0ec1220b761b1c4d5d096406d0cb0d9dd396
yeedaKing <david.yee@ucdenver.edu>
2025-08-05 10:55:20 -0400

    SZ dale merge cross validation

--- graveyard/adagio_standalone.py
+++ graveyard/adagio_standalone.py
@@ -237,8 +237,8 @@ print("'start': {0}\n'end': {1}\n'total time': {2}".format(start, end, end-start
 """
 python3 main.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
-  --genelist '../cross_validation/partitions/RA_STRING/3_RA_new_seeds_0.txt' \
-  --out '../cross_validation/rankings/RA_STRING_dale_merge/3_RA_cross_validation_0.out'
+  --genelist '../cross_validation/partitions/schizophrenia_STRING/3_schizophrenia_new_seeds_0.txt' \
+  --out '../cross_validation/rankings/SZ_STRING_dale_merge/3_SZ_cross_validation_0.out'
 """
 
 
