1b223a22b5253408e1387447a39b029fce91495b
am3li300 <amelie.hopke@tufts.edu>
2025-08-14 10:04:17 -0400

    plan for cross validation script

---
+++ ADAGIO/cross_validate.py
@@ -0,0 +1,13 @@
+"""
+need to get network OR model pickle
+need name of disease
+need partitions subfolder name, default can be STRING
+need method subfolder name
+need to get seed genes and make partitions OR path to partitions folder
+call and run adagio the specified number of times, write output to rankings/method subfolder/disease
+label the rankings based on partitions, write to labels/method subfolder/disease
+call positives on the labels, will have to manually save the graphs
+maybe write positives output to a designated output file with the disease and method name, use write method that lets you add to file instead of overwriting
+all done :)
+
+"""
