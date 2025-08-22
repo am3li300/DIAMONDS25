d7eae5594df1cb9fced20c538dd27140f97e5e9f
yeedaKing <david.yee@ucdenver.edu>
2025-08-22 01:07:52 -0600

    genetic alzheimer's baseline cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -16,10 +16,10 @@ all done :)
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model 'adagio_model' \
-  --disease 'allergy' \
+  --disease 'arthritis' \
   --partition 'STRING' \
   --source 'genetic' \
-  --method 0 \
+  --method 1 \
   --jobs 2 \
   --folds 3
 """
