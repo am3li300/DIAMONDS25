a879234b7411b02523b250962890153f3e83c98d
yeedaKing <david.yee@ucdenver.edu>
2025-08-22 22:25:04 -0600

    genetic bipolar disorder baseline cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -16,7 +16,7 @@ all done :)
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model 'adagio_model' \
-  --disease 'asthma' \
+  --disease 'BD' \
   --partition 'STRING' \
   --source 'genetic' \
   --method 1 \
