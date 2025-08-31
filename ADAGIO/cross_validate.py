cc81945edcb03c9aab2ed560c3afca4cb2242633
yeedaKing <david.yee@ucdenver.edu>
2025-08-30 20:18:57 -0600

    drug CHD disease gene clustering (double merge) cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,11 +2,11 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'COPD' \
+  --disease 'CHD' \
   --partition 'STRING' \
   --source 'drug' \
   --method 2 \
-  --jobs 1 \
+  --jobs 2 \
   --folds 3
 
 python3 cross_validate.py \
