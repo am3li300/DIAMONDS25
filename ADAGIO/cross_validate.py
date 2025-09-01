8300c6c49453cbdd9cd174807de9b0979732f6fe
yeedaKing <david.yee@ucdenver.edu>
2025-08-31 20:44:47 -0600

    drug obesity disease gene clustering (double merge) cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,7 +2,7 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'MS' \
+  --disease 'obesity' \
   --partition 'STRING' \
   --source 'drug' \
   --method 2 \
