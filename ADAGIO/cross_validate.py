6a434cf907296ae5351c9462c7559c63c05ee352
yeedaKing <david.yee@ucdenver.edu>
2025-09-01 11:04:49 -0600

    drug parkinsons, diabetes set, SZ, psoriasis, RA disease gene clustering (double merge) cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,11 +2,11 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'obesity' \
+  --disease 'SZ' \
   --partition 'STRING' \
   --source 'drug' \
   --method 2 \
-  --jobs 2 \
+  --jobs 1 \
   --folds 3
 
 python3 cross_validate.py \
