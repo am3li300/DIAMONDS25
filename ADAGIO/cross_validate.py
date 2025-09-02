c9c26c46141f833088ec578a0195b2a56afa95d5
yeedaKing <david.yee@ucdenver.edu>
2025-09-02 09:41:36 -0600

    drug asthma, BD, cardiac arrhythmia, COPD disease clustering cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,7 +2,7 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'arthritis' \
+  --disease 'COPD' \
   --partition 'STRING' \
   --source 'drug' \
   --method 3 \
