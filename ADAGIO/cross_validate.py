73de7f9bee728677bc5bf539d79b7cab105a684a
yeedaKing <david.yee@ucdenver.edu>
2025-09-02 19:57:19 -0600

    drug diabetes type II disease gene clustering cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,7 +2,7 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'diabetes_type_II' \
+  --disease 'drug_dependence' \
   --partition 'STRING' \
   --source 'drug' \
   --method 3 \
