47363a582cd205303e4258fcc96672ee3cc8be21
yeedaKing <david.yee@ucdenver.edu>
2025-09-05 11:10:20 -0600

    genetic CHD, COPD, diabetes set disease clustering cross validation (25 runs)

--- ADAGIO/cross_validate.py
+++ ADAGIO/cross_validate.py
@@ -2,7 +2,7 @@
 python3 cross_validate.py \
   --network '../data/networks/STRING_protein_links_parsed.tsv' \
   --model '/Users/dkyee/Desktop/adagio_model' \
-  --disease 'cardiac_arrhythmia' \
+  --disease 'diabetes_set' \
   --partition 'STRING' \
   --source 'genetic' \
   --method 3 \
